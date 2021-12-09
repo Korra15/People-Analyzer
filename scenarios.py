aa1d40a407ea9d9eda1052909339505f15081e1a
Hridyakp4 <74726731+Hridyakp4@users.noreply.github.com>
2021-12-10 00:30:20 +0530

    Update scenarios.py

--- scenarios.py
+++ scenarios.py
@@ -10,7 +10,7 @@ scenarios = Blueprint('scenarios', __name__)
 def scenario():
     return render_template('scenarios.html')
 
-
+#Conscientiousness
 @scenarios.route('/Conscientiousness', methods=['GET', 'POST'])
 @login_required
 def consi():
@@ -27,3 +27,50 @@ def consi1():
 @login_required
 def consi2():
     return render_template('consi2.html')
+
+
+#Openness
+@scenarios.route('/Openness', methods=['GET', 'POST'])
+@login_required
+def open():
+    return render_template('openMain.html')
+
+
+@scenarios.route('/Openness101', methods=['GET', 'POST'])
+@login_required
+def open1():
+    return render_template('open1.html')
+
+
+@scenarios.route('/Openness102', methods=['GET', 'POST'])
+@login_required
+def open2():
+    return render_template('open2.html')
+
+
+#Extraversion
+@scenarios.route('/Extraversion', methods=['GET', 'POST'])
+@login_required
+def extra():
+    return render_template('extraMain.html')
+
+
+@scenarios.route('/Extraversion101', methods=['GET', 'POST'])
+@login_required
+def extra1():
+    return render_template('extra1.html')
+
+
+@scenarios.route('/Extraversion102', methods=['GET', 'POST'])
+@login_required
+def extra2():
+    return render_template('extra2.html')
+
+#Agreeableness
+@scenarios.route('/Agreeableness', methods=['GET', 'POST'])
+@login_required
+def agree():
+    return render_template('agreeMain.html')
+
+
+
